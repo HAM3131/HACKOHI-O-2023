8b89c7c3fc49e9f48c17aaeeabc9d61a38119518
Romulus <hamman3131@gmail.com>
2023-10-28 23:40:24 -0400

    fixed up some rendering

--- makeUnion.py
+++ makeUnion.py
@@ -10,7 +10,6 @@ X, Y = np.meshgrid(x,y)
 z = (X+Y)/(2+np.cos(X)*np.sin(Y))*0
 
 img = io.imread('UnionFloor1.jpg',)
-io.imshow(img)
 
 pl_grey =[[0.0, 'rgb(0, 0, 0)'],
     [0.05, 'rgb(13, 13, 13)'],
@@ -52,7 +51,7 @@ surf = go.Surface(x=x, y=y, z=z,
 
 
 
-unionSpace = Space(image=surf)
+unionSpace = Space(image=[surf])
 # {'x':,'y':,'z':,'type':NodeType.}
 data = {'East Main Atrium': {'x':333,'y':233,'z':0,'type':NodeType.ROOM},
         'West Main Atrium': {'x':333,'y':372,'z':0,'type':NodeType.ROOM},
